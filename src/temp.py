f6ed985f0456f45c36c70ae8c5865bcdeacc11b8
Ellise P. <ellise.pagong@gmail.com>
2024-10-30 10:29:37 +0800

    cleaned logic

--- src/temp.py
+++ src/temp.py
@@ -1,4 +1,4 @@
-#This code represents data sent from RasPi module to Output module
+# Code simulates data sent from RasPi module to Output module
 
 import serial
 import time
@@ -10,31 +10,49 @@ root.geometry("600x200")
 valSlider = DoubleVar() 
 valOutput = DoubleVar() 
 present = BooleanVar()
+upper = DoubleVar() # upper boundary of x coordinate
 
-arduino = serial.Serial(port='COM8',  baudrate=115200, timeout=.1)
+upper = 200
+present.set(False)
 
-def write_read(x): # Initialize Arduino
-    arduino.write(bytes(x,  'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return  data
+# arduino = serial.Serial(port='COM8',  baudrate=115200, timeout=.1)
 
-def ArduinoSend(): 
-    if present.get():
+# def write_read(x): # Initialize Arduino
+#     arduino.write(bytes(x,  'utf-8'))
+#     time.sleep(0.05)
+
+
+def getBin(x, high_boundary=upper): # encode x coordinate to binary
+    segment_size = (high_boundary) // 5
+    for i in range(5):
+        low = 1 + i * segment_size
+        high = low + segment_size - 1
+        if i == 5 - 1:
+            high = high_boundary
+        if low <= x <= high:
+            binary_value = format(i + 1, '03b')
+            return binary_value
+
+    return format(0, '03b')  # In case x is outside the range
+
+
+def ardSend(): # send data to arduino
+    if present.get(): 
+        # Output is x coordinate of object given presence of object
         valOutput.set(valSlider.get()) 
     else:
+        #  Output is 0 if there is no object sent
         valOutput.set(0)
-    print("Current value of v1:", valOutput.get())
 
-    #preprocess data before sending to arduino
-    
-    value  = write_read(valOutput.get())
-    print(value)
-    root.after(500, printVal)  # Repeat every 500 ms
+    # encode data before sending to arduino  
+    out = getBin(int(valOutput.get()))
+    # write_read(out) # Send to arduino
+
+    print("Current value: ", valOutput.get()," Current binary: ", out)
+    root.after(500, ardSend)  # Repeat every 500 ms
 
 
-#####################################################################################################################\
-# Code below is purely for GUI purposes #
+## Code below is for GUI purposes ##
 
 def buttPresent():
     present.set(True)
@@ -43,10 +61,7 @@ def notPresent():
     present.set(False)
 
 
-slider = Scale( root, variable = valSlider,  
-           from_ = -100, to = 100,  
-           orient = HORIZONTAL,
-           length=550)    
+slider = Scale( root, variable = valSlider, from_ = 1, to = upper, orient = HORIZONTAL, length=550)    
 
 frame = Frame(root)
 detect = Button(frame, text="object present", command=buttPresent)   
@@ -62,6 +77,5 @@ slider.pack(anchor=CENTER, expand=True)
 label1.pack(anchor=CENTER, pady=10) 
 frame.pack(anchor=CENTER, pady=10)
 
-
-printVal()
+ardSend()
 root.mainloop()     
