9e906e87f2b467813f620e047d924c4e8e603dbe
Ellise P. <ellise.pagong@gmail.com>
2025-02-03 13:28:56 +0800

    Out mod (#1)
    * Working logic for temp
    
    * cleaned logic
    
    * Arduino communication code
    
    * Added best pt model
    
    * new pt model
    
    * improved inference code (no class mapping)
    
    * Added class mapping and Raspi PWM logic
    
    * added comments
    
    * Code setup for raspi simulation
    
    * Working x axis indication
    
    * Included class in data sent via serial
    
    * Changes to arduino communication logic
    
    * Final computer vision logic in Ultralytics
    
    * Added weighted scoring system and cmos data handling
    
    * Added command for motor testing

--- src/main.py
+++ src/main.py
@@ -1,56 +1,106 @@
 #documentation: https://www.ultralytics.com/blog/object-detection-with-a-pre-trained-ultralytics-yolov8-model
 
-#todo
-#   dataLoader class for custom dataset
-#   model class for trained model (maybe, merge with train class)
-#   trainer class for transfer learning/retraining
-#   predictor class for prediction and evaluation
-
+# Sends largest object to arduino
+# This is assuming the largest object is the closest as indicated by TOF sensor
+# Additional logic will be used once TOF output is confirmed
 
 from ultralytics import YOLO
 import cv2, torch
+import serial
+
+arduino = serial.Serial(port='COM5',  baudrate=9600, timeout=1)                                                         # Starts connection with Arduino
+
+classes_dict = {
+    0: 'animal',
+    1: 'barrier',
+    2: 'bike',
+    3: 'crosswalk',
+    4: 'hazard-sign',
+    5: 'person',
+    6: 'pole',
+    7: 'stairs',
+    8: 'stall',
+    9: 'vehicle'
+}
+
+max_width = 720                                                                                                         # video pixel width
+seg_size = max_width / 5
+
+def send_to_arduino(largest_boxes):
+    classes_message = [
+        int(data[2].item()) if data is not None else "-1"                                                               # Use -1 for segments with no valid box
+        for data in largest_boxes.values()
+    ]
+    message = ",".join(map(str, classes_message)) + "\n"                                                                # Join the list into a single string separated by commas
+    arduino.write(message.encode())                                                                                     # Send the complete message to Arduino
+    # print(f"Sent: {message.strip()}")                                                                                   # Log the sent message
+
+def display_pred(img, largest_box):                                                                                     # displays bounding box
+    for data in largest_box.values():
+        if data is not None:
+            box, conf, cls = data
+            x1, y1, x2, y2 = box
+
+            cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 50, 50), 2)  # Bounding box
+            text = f'Class: {classes_dict[int(cls)]}, Conf: {conf:.2f}'
+            (text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+            rect_start = (int(x1), int(y1) - text_height - 10)
+            rect_end = (int(x1) + text_width, int(y1))
+            cv2.rectangle(img, rect_start, rect_end, (255, 50, 50), -1)  # Class label box
+            cv2.putText(img, text, (int(x1), int(y1) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255),
+                        1)  # Class text
+
+def assign_segment(x1, x2):                                                                                             #Assigns object to image segment
+    x = ((x2 - x1) / 2) + x1
+    segment_index = int(x // seg_size)
+    if segment_index < 0:
+        segment_index = 0
+    elif segment_index >= 5:
+        segment_index = 4
+    return segment_index
 
 def main():
-    torch.cuda.set_device(0)  #use gpu
+    torch.cuda.set_device(0)
 
-    model = YOLO('yolov8n.pt') #load pretrained dataset
-    videoStream = cv2.VideoCapture(1) # camera feed
-
-    if not videoStream.isOpened():
-        print('Error in camera')
-        exit(0)
+    # video_stream = cv2.VideoCapture(1)                                                                                # camera feed
+    video_stream = cv2.VideoCapture("testvid.mp4")                                                                      # video source
+    model = YOLO('model.pt')
 
     while True:
-        ret, img = videoStream.read()
+        ret, img = video_stream.read()
+        img = cv2.resize(img, (720, 480))
         if not ret:
             print("Failed to grab frame")
             break
 
-        # Perform inference on the current frame
-        results = model(source=img, show=True, conf=0.4, save=True)  # Predict using the model
+        results = model(source=img, show=False, conf=0.5, save=True)                                                    # Perform inference on the current frame
 
-        # classes returns as integers instead of class names; remove save=True from above to use this
-        # for result in results:
-        #     boxes = result.boxes.xyxy  # Get bounding box coordinates (x1, y1, x2, y2)
-        #     confs = result.boxes.conf  # Get confidence scores
-        #     classes = result.boxes.cls  # Get class indices
-        #
-        #     for box, conf, cls in zip(boxes, confs, classes):
-        #         x1, y1, x2, y2 = box
-        #         cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)  # Draw bounding box
-        #         cv2.putText(img, f'Class: {int(cls)}, Conf: {conf:.2f}', (int(x1), int(y1) - 10),
-        #                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-        #
-        # # Show the frame with predictions
-        # cv2.imshow("Live Video", img)
+        for result in results:
+            boxes = result.boxes.xyxy
+            confs = result.boxes.conf
+            classes = result.boxes.cls
 
-        # Break the loop on 'ESC' key press
+            largest_boxes = {i: None for i in range(5)}
+            largest_areas = {i: 0 for i in range(5)}
+
+            for box, conf, cls in zip(boxes, confs, classes):
+                x1, y1, x2, y2 = box
+                area = (x2 - x1) * (y2 - y1)
+                if classes_dict[int(cls)] == "pole":                                                                    # Scale down poles due to generally large size
+                    area *= 0.3
+
+                seg = assign_segment(x1, x2)
+                if area > largest_areas[seg]:                                                                           # Finds largest object in each segment
+                    largest_areas[seg] = area
+                    largest_boxes[seg] = (box, conf, cls)
+
+            display_pred(img, largest_boxes)                                                                            # Display largest object in each segment
+            send_to_arduino(largest_boxes)                                                                              # Send largest objects to Arduino
+
+        cv2.imshow("Video", img)
+
         if cv2.waitKey(10) == 27:
-            break
-
-    # Release the video stream and close windows
-    videoStream.release()
-    cv2.destroyAllWindows()
+                break
 
 if __name__ == '__main__':
     main()
---
+++ src/modOutput/temp.py
@@ -0,0 +1,79 @@
+# Code simulates data sent from RasPi module to Output module
+
+import serial
+import time
+from tkinter import *
+
+# GUI variables
+root = Tk()   
+root.geometry("600x200")  
+valSlider = DoubleVar() 
+valOutput = DoubleVar() 
+present = BooleanVar()
+upper = DoubleVar() # upper boundary of x coordinate
+
+upper = 200
+present.set(False)
+
+arduino = serial.Serial(port='COM8',  baudrate=115200, timeout=.1)
+
+def write_read(x): # Initialize Arduino
+    arduino.write(bytes(x,  'utf-8'))
+    time.sleep(0.05)
+
+def getBin(x, high_boundary=upper): # encode x coordinate to binary
+    segment_size = (high_boundary) // 5
+    for i in range(5):
+        low = 1 + i * segment_size
+        high = low + segment_size - 1
+        if i == 5 - 1:
+            high = high_boundary
+        if low <= x <= high:
+            binary_value = format(i + 1, '03b')
+            return binary_value
+
+    return format(0, '03b')  # In case x is outside the range
+
+
+def ardSend(): # send data to arduino
+    if present.get(): 
+        # Output is x coordinate of object given presence of object
+        valOutput.set(valSlider.get()) 
+    else:
+        #  Output is 0 if there is no object sent
+        valOutput.set(0)
+
+    # encode data before sending to arduino  
+    out = getBin(int(valOutput.get()))
+    write_read(out) # Send to arduino
+    print("Current value: ", valOutput.get()," Current binary: ", out)
+    root.after(500, ardSend)  # Repeat every 500 ms
+
+
+## Code below is for GUI purposes ##
+
+def buttPresent():
+    present.set(True)
+
+def notPresent():
+    present.set(False)
+
+
+slider = Scale( root, variable = valSlider, from_ = 1, to = upper, orient = HORIZONTAL, length=550)    
+
+frame = Frame(root)
+detect = Button(frame, text="object present", command=buttPresent)   
+empty = Button(frame, text="no object", command=notPresent)   
+label = Label(root, text="Slider represents x coordinate of detected object (data from RasPi sent to arduino)") 
+label1 = Label(root, text="Button represents whether there is an object (no object detected = raspi has no data to send to arduino)") 
+          
+detect.pack(side=LEFT, padx=5)
+empty.pack(side=LEFT, padx=5)
+
+label.pack(anchor=CENTER, pady=10) 
+slider.pack(anchor=CENTER, expand=True)  
+label1.pack(anchor=CENTER, pady=10) 
+frame.pack(anchor=CENTER, pady=10)
+
+ardSend()
+root.mainloop()     
